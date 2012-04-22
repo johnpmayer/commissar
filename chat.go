3770a317f628f9602a4befd30cb1696655a8dd28
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2012-04-22 13:27:49 -0400

    added message multiplexing, works until a client disconnects (causes server crash)

--- chat.go
+++ chat.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 	//	"github.com/hoisie/web"
 	"code.google.com/p/go.net/websocket"
-//	"io"
+	//	"io"
 	"web"
 )
 
 func echo(ws *websocket.Conn) {
 
 	fmt.Println("Opened websocket")
-
+	
 	for {
-		
+
 		var msg string
 		var err error
-		
+
 		err = websocket.Message.Receive(ws, &msg)
 		check(err)
-		
+
 		fmt.Println(msg)
-		
+
 		err = websocket.Message.Send(ws, msg)
 		check(err)
-		
+
 	}
 
 	fmt.Println("Finished copying websocket")
@@ -37,5 +37,57 @@ func upgradeWebsocketHandler(wsHandler websocket.Handler) interface{} {
 }
 
 func chatServer() interface{} {
-	return upgradeWebsocketHandler(websocket.Handler(echo))
+
+	chatMux := make(chan string)
+	listeners := make([](chan string), 0, 10)
+
+	go func() {
+
+		for {
+
+			msg := <-chatMux
+
+			// TODO: switch on adding a new listener
+
+			for i := 0; i < len(listeners); i += 1 {
+
+				listeners[i] <- msg
+
+			}
+
+		}
+
+	}()
+	
+	broadcast := func(ws *websocket.Conn) {
+		
+		rcv := make(chan string)
+		
+		listeners = append(listeners, rcv)
+		
+		go func() {
+			
+			for {
+				
+				msg := <- rcv
+				err := websocket.Message.Send(ws, msg)
+				check(err)
+				
+			}
+			
+		}()
+		
+		for {
+			
+			var msg string
+			err := websocket.Message.Receive(ws, &msg)
+			check(err)
+			
+			chatMux <- msg
+			
+		}
+		
+	}
+	
+	return upgradeWebsocketHandler(websocket.Handler(broadcast))
 }
